506006d8c958568756b5858c5c81214609568d89
Alies Lapatsin <web.lapateen@gmail.com>
2022-07-18 18:40:07 +0300

    Add missing PHPDoc for Dialogs Facade

--- src/Laravel/Facades/Dialogs.php
+++ src/Laravel/Facades/Dialogs.php
@@ -7,6 +7,8 @@ use KootLabs\TelegramBotDialogs\DialogManager;
 
 /**
  * @method \KootLabs\TelegramBotDialogs\DialogManager bot(\Telegram\Bot\Api $bot)
+ * @method void activate(\KootLabs\TelegramBotDialogs\Dialog $dialog)
+ * @method void proceed(\Telegram\Bot\Objects\Update $update)
  * @mixin \KootLabs\TelegramBotDialogs\DialogManager
  */
 final class Dialogs extends Facade
