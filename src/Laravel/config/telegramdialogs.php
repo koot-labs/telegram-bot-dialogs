c92c967f9a85834559ddbc21a43bb63739c1dc58
Alies Lapatsin <web.lapateen@gmail.com>
2024-05-12 17:05:17 +0200

    Add `cache_tag` config key

--- src/Laravel/config/telegramdialogs.php
+++ src/Laravel/config/telegramdialogs.php
@@ -11,4 +11,12 @@ return [
             'connection' => env('TELEGRAM_DIALOGS_REDIS_CONNECTION', 'default'),
         ],
     ],
+
+    /*
+     * If the cache driver you configured supports tags, you may specify a tag name here.
+     * All stored dialogs will be tagged. When clearing, the dialogs only items with that tag will be flushed.
+     * @see https://laravel.com/docs/9.x/cache#cache-tags
+     * You may use a string or an array here.
+     */
+    'cache_tag' => '',
 ];
